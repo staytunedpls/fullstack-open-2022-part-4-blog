f23e9654256c970bc49f74cd914a2186c94a95d0
Sergey Starikov <sergeystarikov96@gmail.com>
2022-08-09 09:19:10 +0400

    Moved logger to separate module

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const BlogsRouter = require("./controllers/blogs");
+const logger = require('./utils/logger')
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    console.log("connected to MongoDB");
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    console.error("error connection to MongoDB:", error.message);
+    logger.error("error connection to MongoDB:", error.message);
   });
 
 app.use(cors());
