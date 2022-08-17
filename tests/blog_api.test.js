11436ad8c176c91ec9afcd29e198e4fb06caa4f2
Sergey Starikov <sergeystarikov96@gmail.com>
2022-08-17 09:47:20 +0400

    Done 4.11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -34,8 +34,23 @@ test("POST works for correct blogs", async () => {
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/);
-  const blogsAfter = await helper.blogsInDb()
-  expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1)
+  const blogsAfter = await helper.blogsInDb();
+  expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1);
+});
+
+test("Default likes is 0", async () => {
+  const newBlog = {
+    title: "Blog no likes info",
+    author: "Author no likes info",
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const blogsAfter = await helper.blogsInDb();
+  expect(blogsAfter[blogsAfter.length - 1].likes).toBe(0);
 });
 
 afterAll(() => {
