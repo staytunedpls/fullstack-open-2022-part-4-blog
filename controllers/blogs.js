5df2fc2d0eb210a3f02789167a3313f0030efc82
Sergey Starikov <sergeystarikov96@gmail.com>
2022-08-24 10:59:32 +0400

    Done 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,27 +1,41 @@
+const jwt = require("jsonwebtoken");
 const BlogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
 BlogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate("user", {username: 1, name: 1, _id: 1});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    _id: 1,
+  });
   response.json(blogs);
 });
 
 BlogsRouter.post("/", async (request, response) => {
-  const { title, url, author, likes } = request.body;
-  const users = await User.find({});
-  const selectedUser = users[0];
+  const { token, title, url, author, likes } = request.body;
+  const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+  console.log(decodedToken);
+
+  if (!decodedToken.id) {
+    response.status(401).json({ error: "Invalid token" });
+  }
+  const decodedUser = await User.findById(decodedToken.id);
+
   const blog = new Blog({
     title,
     url,
     author,
     likes,
-    user: selectedUser._id,
+    user: decodedUser._id,
   });
-  
+
   const savedBlog = await blog.save();
-  selectedUser.blogs = selectedUser.blogs.concat(savedBlog._id)
-  await selectedUser.save();
+  if (!decodedUser.blogs) {
+    decodedUser.blogs = [];
+  }
+  decodedUser.blogs = decodedUser.blogs.concat(savedBlog._id);
+  await decodedUser.save();
 
   response.status(201).json(savedBlog);
 });
